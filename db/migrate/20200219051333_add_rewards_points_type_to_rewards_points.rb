28535eb7a264d5d3219d20ace81f857b1d35594e
Ben Muschol <ben.muschol@airbnb.com>
2020-02-18 21:39:14 -0800

    Add a default'

--- db/migrate/20200219051333_add_rewards_points_type_to_rewards_points.rb
+++ db/migrate/20200219051333_add_rewards_points_type_to_rewards_points.rb
@@ -1,5 +1,5 @@
 class AddRewardsPointsTypeToRewardsPoints < ActiveRecord::Migration[6.0]
   def change
-    add_reference :rewards_points, :rewards_points_type, null: false, foreign_key: true
+    add_reference :rewards_points, :rewards_points_type, null: false, foreign_key: true, default: 1
   end
 end
